51d71f3f3432b9ac7eeeae16e8d9db1d146401ca
CB6 <melissa.j.womack@gmail.com>
2018-09-24 18:04:44 -0500

    Set up routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,35 @@
+const express = require("express");
+const router = express.Router();
 const path = require("path");
-const router = require("express").Router();
-const apiRoutes = require("./searchRoutes");
 
-// API Routes
-router.use("/api", apiRoutes);
+const db = require("../models");
+
+router.get("/saved", function (req, res) {
+  db.Article.find({})
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+});
+
+router.post("/saved", function (req, res) {
+  const newArticle = {
+    title: req.body.title,
+    url: req.body.url,
+    date: req.body.date
+  };
+  db.Article.create(newArticle)
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+});
+
+router.delete("/saved/:id", function (req, res) {
+  console.log("Deleting id " + req.params.id);
+  db.Article.remove({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+});
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
+router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,7 +1,32 @@
-const router = require("express").Router();
-const articleRoutes = require("./api/articles");
+require('dotenv').config();
+const express = require("express");
+const router = express.Router();
+const request = require("request");
 
-// Book routes
-router.use("/articles", articleRoutes);
+router.get("/", function(req,res) {
+    const { searchTerm, startDate, endDate } = req.query;
+    const apiKey = process.env.NYT_API_KEY;
+  
+    let queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${searchTerm}`;
+    if (startDate) queryUrl += `&begin_date=${startDate}`;
+    if (endDate) queryUrl += `&end_date=${endDate}`;
+    console.log(queryUrl);
+  
+    request(queryUrl, function(err,response,body) {
+      if (err) {
+        console.log(err + response);
+        return res.json(err);
+      };
+      const rawJson = JSON.parse(body);
+      const articleData = rawJson.response.docs.map(article => {
+        return {
+          title: article.headline.main,
+          url: article.web_url,
+          date: article.pub_date || "Not provided"
+        };
+      });
+      res.json(articleData);
+    });
+});
 
 module.exports = router;
