1a723ed2a06ed270da862a2914a0717012682849
CB6 <melissa.j.womack@gmail.com>
2018-09-24 17:55:05 -0500

    Setup server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = requre("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -11,15 +12,35 @@ app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1/reactreadinglist"
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact"
 );
+
+const db = mongoose.connection;
+db.on('error', function (err) {
+  console.log("-----Mongoose error: -----\n" + err);
+});
+db.once('open', function () {
+  console.log("Mongoose connected successfully");
+});
+
+//Import API Routes
+const apiRoutes = require("./routes/apiRoutes.js");
+const searchRoutes = require("./routes/searchRoutes.js");
+app.use("/api", apiRoutes);
+app.use("/search", searchRoutes);
+
+//React routing
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // Start the API server
 app.listen(PORT, function() {
